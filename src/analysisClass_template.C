ade946e700f91f0a224f8a6221db88a87da829a3
prumerio <>
2008-11-27 16:17:07 +0000

    Example from UserCode/Leptoquarks/rootNtupleAnalyzer/src/analysisClass_template.C

---
+++ src/analysisClass_template.C
@@ -0,0 +1,45 @@
+#define analysisClass_cxx
+#include "analysisClass.h"
+
+analysisClass::analysisClass(string * inputList, string * treeName, TString * outputFileName)
+  :baseClass(inputList, treeName, outputFileName)
+{
+  std::cout << "analysisClass::analysisClass(): begins " << std::endl;
+
+  std::cout << "analysisClass::analysisClass(): ends " << std::endl;
+}
+
+analysisClass::~analysisClass()
+{
+  std::cout << "analysisClass::~analysisClass(): begins " << std::endl;
+
+  std::cout << "analysisClass::~analysisClass(): ends " << std::endl;
+}
+
+void analysisClass::Loop()
+{
+   std::cout << "analysisClass::Loop() begins" <<std::endl;   
+   
+   if (fChain == 0) return;
+   
+   Long64_t nentries = fChain->GetEntriesFast();
+   std::cout << "analysisClass::Loop(): nentries = " << nentries << std::endl;   
+
+   ////// The following ~7 lines have been taken from rootNtupleClass->Loop() /////
+   ////// If the root version is updated and rootNtupleClass regenerated,     /////
+   ////// these lines may need to be updated.                                 /////    
+   Long64_t nbytes = 0, nb = 0;
+   for (Long64_t jentry=0; jentry<nentries;jentry++) {
+     Long64_t ientry = LoadTree(jentry);
+     if (ientry < 0) break;
+     nb = fChain->GetEntry(jentry);   nbytes += nb;
+     if(jentry < 10 || jentry%100 == 0) std::cout << "analysisClass::Loop(): jentry = " << jentry << std::endl;   
+     // if (Cut(ientry) < 0) continue;
+
+     ////////////////////// User's code starts here ///////////////////////
+
+
+     ////////////////////// User's code ends here ///////////////////////
+   }
+   std::cout << "analysisClass::Loop() ends" <<std::endl;   
+}
