737f6dbe225f2ed18f67d8a86f080b5d55caa8d4
lockner <>
2009-01-25 21:53:21 +0000

    added Photon25 to list of studied triggers

--- src/analysisClass_HLTStudies.C
+++ src/analysisClass_HLTStudies.C
@@ -44,17 +44,18 @@ void analysisClass::Loop()
    TH1F *h_ele_Eta_200  = new TH1F ("ele_Eta_200","ele_Eta_200",121,-6.05,6.05);
    TH1F *h_ele_CaloEnergy_200  = new TH1F ("ele_CaloEnergy_200","ele_CaloEnergy_200",100,0,1000);
 
-   TH1F *h_ele_E_SingEle = new TH1F ("ele_E_SingEle","ele_E_SingEle",100,0,1000);
-   TH1F *h_ele_Pt_SingEle  = new TH1F ("ele_Pt_SingEle","ele_Pt_SingEle",100,0,1000);
-   TH1F *h_ele_Phi_SingEle  = new TH1F ("ele_Phi_SingEle","ele_Phi_SingEle",71,-3.55,3.55);
-   TH1F *h_ele_Eta_SingEle  = new TH1F ("ele_Eta_SingEle","ele_Eta_SingEle",121,-6.05,6.05);
-   TH1F *h_ele_CaloEnergy_SingEle  = new TH1F ("ele_CaloEnergy_SingEle","ele_CaloEnergy_SingEle",100,0,1000);
+   TH1F *h_ele_E_Photon = new TH1F ("ele_E_Photon","ele_E_Photon",100,0,1000);
+   TH1F *h_ele_Pt_Photon  = new TH1F ("ele_Pt_Photon","ele_Pt_Photon",100,0,1000);
+   TH1F *h_ele_Phi_Photon  = new TH1F ("ele_Phi_Photon","ele_Phi_Photon",71,-3.55,3.55);
+   TH1F *h_ele_Eta_Photon  = new TH1F ("ele_Eta_Photon","ele_Eta_Photon",121,-6.05,6.05);
+   TH1F *h_ele_CaloEnergy_Photon  = new TH1F ("ele_CaloEnergy_Photon","ele_CaloEnergy_Photon",100,0,1000);
 
 
    /////////initialize variables
    double EventsPassed_80=0;
    double EventsPassed_200=0;
-   double EventsPassed_SingEle=0;
+   double EventsPassed_80_OR_200=0;
+   double EventsPassed_Photon=0;
    double n_Events=0;
 
    int electron_PID=11;
@@ -70,8 +71,8 @@ void analysisClass::Loop()
    ////// If the root version is updated and rootNtupleClass regenerated,     /////
    ////// these lines may need to be updated.                                 /////    
    Long64_t nbytes = 0, nb = 0;
-   //for (Long64_t jentry=0; jentry<nentries;jentry++) {
-   for (Long64_t jentry=0; jentry<1;jentry++) {
+   for (Long64_t jentry=0; jentry<nentries;jentry++) {
+   //for (Long64_t jentry=0; jentry<1;jentry++) {
      Long64_t ientry = LoadTree(jentry);
      if (ientry < 0) break;
      nb = fChain->GetEntry(jentry);   nbytes += nb;
@@ -82,7 +83,7 @@ void analysisClass::Loop()
 
      string Trig1="HLT_EM80";
      string Trig2="HLT_EM200";
-     string Trig3="HLT_IsoEle18_L1R";  //??This doesn't seem to be like the SingleElec
+     string Trig3="HLT_Photon25"; 
 
      string tmp="";
      int results_index=0;
@@ -93,9 +94,10 @@ void analysisClass::Loop()
 	 //cout << tmp << "   " << HLTResults[results_index] << endl;
 	 bool IsEM80 =  !strncmp(Trig1.c_str(),tmp.c_str(),8); // returns zero if first 8 characters in 2 strings are same
 	 bool IsEM200 = !strncmp(Trig2.c_str(),tmp.c_str(),9);
-	 bool IsSingEle = !strncmp(Trig3.c_str(),tmp.c_str(),12);
+	 bool IsPhoton = !strncmp(Trig3.c_str(),tmp.c_str(),11);
 	 //if ((IsEM80)||(IsEM200)) cout << "Trigger:  " << tmp << "  HLTResults " << HLTResults[results_index] << endl;
-	 if (IsSingEle) cout << "Trigger:  " << tmp << "  HLTResults " << HLTResults[results_index] << endl;
+	 //if (IsPhoton) cout << "Trigger:  " << tmp << "  HLTResults " << HLTResults[results_index] << endl;
+
 	 if ((IsEM80)&&(HLTResults[results_index]==true)) {
 	   EventsPassed_80++;
 	   for (int iele=0; iele<eleCount; iele++) {
@@ -121,16 +123,20 @@ void analysisClass::Loop()
 	   } //for iele	   
 	 }
 
-	 if ((IsSingEle)&&(HLTResults[results_index]==true)) {
-	   EventsPassed_SingEle++;
+	 if ((IsPhoton)&&(HLTResults[results_index]==true)) {
+	   EventsPassed_Photon++;
 	   for (int iele=0; iele<eleCount; iele++) {
 	     if (elePt[iele]<elePt_cut) continue;
-	     h_ele_E_SingEle->Fill(eleEnergy[iele]);
-	     h_ele_Pt_SingEle->Fill(elePt[iele]);
-	     h_ele_Phi_SingEle->Fill(elePhi[iele]);
-	     h_ele_Eta_SingEle->Fill(eleEta[iele]);
-	     h_ele_CaloEnergy_SingEle->Fill(eleCaloEnergy[iele]);
+	     h_ele_E_Photon->Fill(eleEnergy[iele]);
+	     h_ele_Pt_Photon->Fill(elePt[iele]);
+	     h_ele_Phi_Photon->Fill(elePhi[iele]);
+	     h_ele_Eta_Photon->Fill(eleEta[iele]);
+	     h_ele_CaloEnergy_Photon->Fill(eleCaloEnergy[iele]);
 	   } //for iele	   
+	 }
+
+	 if (((IsEM80)&&(HLTResults[results_index]==true))||((IsEM80)&&(HLTResults[results_index]==true))) {
+	   EventsPassed_80_OR_200++;
 	 }
 
 	 tmp.clear(); //reset temporary string of HLT name
@@ -156,16 +162,17 @@ void analysisClass::Loop()
    h_ele_Eta_200->Write();
    h_ele_CaloEnergy_200->Write();
 
-   h_ele_E_SingEle->Write();
-   h_ele_Pt_SingEle->Write();
-   h_ele_Phi_SingEle->Write();
-   h_ele_Eta_SingEle->Write();
-   h_ele_CaloEnergy_SingEle->Write();
+   h_ele_E_Photon->Write();
+   h_ele_Pt_Photon->Write();
+   h_ele_Phi_Photon->Write();
+   h_ele_Eta_Photon->Write();
+   h_ele_CaloEnergy_Photon->Write();
 
 
    cout << "Events Passed Trigger EM_80: " << EventsPassed_80 << "  Fraction: " << EventsPassed_80/n_Events << endl;
    cout << "Events Passed Trigger EM_200: " << EventsPassed_200 << "  Fraction: " << EventsPassed_200/n_Events <<endl;
-   cout << "Events Passed Trigger SingEle: " << EventsPassed_SingEle << "  Fraction: " << EventsPassed_SingEle/n_Events <<endl;
+   cout << "Events Passed Trigger EM_80 OR EM_200: " << EventsPassed_80_OR_200 << "  Fraction: " << EventsPassed_80_OR_200/n_Events << endl;
+   cout << "Events Passed Trigger Photon: " << EventsPassed_Photon << "  Fraction: " << EventsPassed_Photon/n_Events <<endl;
 
    std::cout << "analysisClass::Loop() ends" <<std::endl;   
 }
